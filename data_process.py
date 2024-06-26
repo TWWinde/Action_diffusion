63c5f9b55b66cf023f727c864d0e0119f3897f89
TWWinde <twwinde@gmail.com>
2024-06-26 15:00:50 +0200

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -14,15 +14,13 @@ def read_videos_and_step_start_end(root_path):
     csv_path = []
     vid_path = []
     for i in range(len(video_names)):
-        csv_path.append(os.path.join(csv_root_path, video_names + 'csv'))
-        video_type = video_names.split('_')[0:1]
-        vid_path.append(os.path.join(root_path, video_type, 'videos', video_names + '.mpg'))
+        csv_path.append(os.path.join(csv_root_path, video_names[i] + '.csv'))
+        video_type = video_names[i].split('_')[0]
+        vid_path.append(os.path.join(root_path, video_type, 'videos', video_names[i] + '.mpg'))
 
     print(csv_path)
-
     print(vid_path)
     return csv_path, vid_path
-
 
 if __name__ =='__main__':
     read_videos_and_step_start_end('/scratch/users/tang/data/niv')
