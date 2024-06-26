7a379345f23ac14e09ac1d02059668b29ffc9ffc
TWWinde <twwinde@gmail.com>
2024-06-26 15:11:40 +0200

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -1,6 +1,8 @@
 import glob
 import os
 
+import pandas as pd
+
 
 def read_videos_and_step_start_end(root_path):
 
@@ -18,9 +20,17 @@ def read_videos_and_step_start_end(root_path):
         video_type = video_names[i].replace('_' + video_names[i].split('_')[-1], '')
         vid_path.append(os.path.join(root_path, video_type, 'videos', video_names[i] + '.mpg'))
 
-    print(csv_path)
-    print(vid_path)
     return csv_path, vid_path
 
+
+def read_csv(path):
+    df = pd.read_csv(path)
+
+    print(df)
+
+
+
 if __name__ =='__main__':
-    read_videos_and_step_start_end('/scratch/users/tang/data/niv')
+
+    csv_path, vid_path = read_videos_and_step_start_end('/scratch/users/tang/data/niv')
+    read_csv(csv_path[7])
