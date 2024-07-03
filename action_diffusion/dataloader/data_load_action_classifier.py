b12be9d05a77aeee1da46b284e546463c1a99fa6
TWWinde <twwinde@gmail.com>
2024-07-03 14:42:22 +0200

    Update data_load_action_classifier.py

--- action_diffusion/dataloader/data_load_action_classifier.py
+++ action_diffusion/dataloader/data_load_action_classifier.py
@@ -13,23 +13,28 @@ class ActionDataset(Dataset):
         self.file_path_list = [os.path.join(self.data_root, name) for name in self.file_name_list if name.endswith('.npy')]
         self.num_classes = 48
         self.data = []
-        self._load_data()
+        self.index_map = self._create_index_map()
 
-    def _load_data(self):
-        for d in self.file_path_list:
-            path = os.path.join(self.data_root, d)
-            data = np.load(path, allow_pickle=True)
-            for info in data:
-                action_labels = info['steps_ids']
-                video_feature = info['video_feature']
-                text_feature = info['text_feature']
-                self.data.append((action_labels, video_feature, text_feature))
+    def _create_index_map(self):
+        index_map = []
+        for file_idx, file_path in enumerate(self.file_path_list):
+            data = np.load(file_path, allow_pickle=True)
+            for sample_idx in range(len(data)):
+                index_map.append((file_idx, sample_idx))
+        return index_map
 
     def __len__(self):
-        return len(self.data)
+        return len(self.index_map)
 
     def __getitem__(self, idx):
-        action_labels, video_feature, text_feature = self.data[idx]
+        file_idx, sample_idx = self.index_map[idx]
+        file_path = self.file_path_list[file_idx]
+        data = np.load(file_path, allow_pickle=True)
+        info = data[sample_idx]
+
+        action_labels = info['steps_ids']
+        video_feature = info['video_feature']
+        text_feature = info['text_feature']
         action_labels = torch.tensor(action_labels, dtype=torch.long)
         action_labels = F.one_hot(action_labels, num_classes=self.num_classes).float()
         video_feature = torch.tensor(video_feature, dtype=torch.float32)
