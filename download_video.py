57e96378f4774aa45917536c9dbabaace2830167
TWWinde <twwinde@gmail.com>
2024-07-09 16:13:40 +0200

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -1,5 +1,7 @@
 import csv
 import os
+
+import numpy as np
 import yt_dlp
 
 
@@ -16,13 +18,20 @@ def read_videos(file_path):
     return videos
 
 
-def download_video(video_url, output_path):
+def download_video(video_url, output_path, video):
+    fail_case=[]
     ydl_opts = {
         'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
         'format': 'best',
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([video_url])
+        try:
+            ydl.download([video_url])
+        except:
+            print(video_url, 'is not available')
+            fail_case.append(video)
+
+    np.save('/scratch/users/tang/data/crosstask_release/', 'fail_case.py')
 
 
 videos = read_videos('/scratch/users/tang/data/crosstask_release/videos.csv')
@@ -31,4 +40,4 @@ output_dir = '/scratch/users/tang/data/crosstask_release/videos'
 os.makedirs(output_dir, exist_ok=True)
 
 for video in videos:
-    download_video(video['url'], output_dir)
+    download_video(video['url'], output_dir, video)
