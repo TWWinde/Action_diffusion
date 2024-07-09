840525a03e85f1da95c83d75db017c1c16068a4e
TWWinde <twwinde@gmail.com>
2024-07-09 16:05:16 +0200

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -2,6 +2,7 @@ import csv
 import os
 import yt_dlp
 
+
 def read_videos(file_path):
     videos = []
     with open(file_path, 'r') as file:
@@ -14,6 +15,7 @@ def read_videos(file_path):
             })
     return videos
 
+
 def download_video(video_url, output_path):
     ydl_opts = {
         'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
@@ -22,9 +24,10 @@ def download_video(video_url, output_path):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
 
+
 videos = read_videos('/scratch/users/tang/data/crosstask_release/videos.csv')
 
-output_dir = '/scratch/users/tang/data/crosstask/videos'
+output_dir = '/scratch/users/tang/data/crosstask_release/videos'
 os.makedirs(output_dir, exist_ok=True)
 
 for video in videos:
