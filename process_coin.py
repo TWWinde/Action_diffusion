05c66571f3d65de62dd529a91d0f3d22ff330e50
TWWinde <twwinde@gmail.com>
2024-07-11 13:38:09 +0200

    Update process_coin.py

--- process_coin.py
+++ process_coin.py
@@ -9,5 +9,5 @@ with open(json_path, 'r') as f:
         raise ValueError("JSON file is empty")
     data = json.loads(file_content)
 
-for video in data['database'].items():
-    print(f"Video ID: {video}")
+for video_id, video_info in data['database'].items():
+    print(f"Video ID: {video_id}")
