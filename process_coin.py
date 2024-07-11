894ab350ad4a53c4942022b106cc3ce6110c7317
TWWinde <twwinde@gmail.com>
2024-07-11 13:33:18 +0200

    Update process_coin.py

--- process_coin.py
+++ process_coin.py
@@ -3,7 +3,11 @@ import json
 
 
 json_path = "/scratch/users/tang/data/COIN/COIN.json"
-data = json.loads(json_path)
+with open(json_path, 'r') as f:
+    file_content = f.read()
+    if not file_content.strip():
+        raise ValueError("JSON file is empty")
+    data = json.loads(file_content)
 
 for video_id in data['database'].items():
     print(f"Video ID: {video_id}")
