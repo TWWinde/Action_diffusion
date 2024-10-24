48b697ffa84d030ccbc49c27185797f3603bed74
Wenwu Tang <tang@gpuws3.hcics.simtech.uni-stuttgart.de>
2024-10-24 17:04:13 +0200

    change

--- action_diffusion/train_action_classifier.sh
+++ action_diffusion/train_action_classifier.sh
@@ -1,5 +1,5 @@
 python3 /scratch/users/tang/Action_diffusion/action_diffusion/action_classifier.py \
---root /scratch/users/tang/data/niv/processed_data_16_onlystart_pooled_aug_x6 \
+--root /scratch/users/tang/data/niv/train_16_onlystart_pooled_aug_x6 \
 --num_thread_reader 8 \
 --cudnn_benchmark 1 \
 --pin_memory \
@@ -14,33 +14,33 @@ python3 /scratch/users/tang/Action_diffusion/action_diffusion/action_classifier.
 --dataset niv
 
 
-python3 /scratch/users/tang/Action_diffusion/action_diffusion/action_classifier.py \
---root /scratch/users/tang/data/COIN/* \
---num_thread_reader 8 \
---cudnn_benchmark 1 \
---pin_memory \
---checkpoint_dir whl \
---batch_size 256 \
---batch_size_val 256 \
---evaluate \
---resume \
---horizon 3 \
---class_dim 778 \
---observation_dim 768 \
---dataset coin
+#python3 /scratch/users/tang/Action_diffusion/action_diffusion/action_classifier.py \
+#--root /scratch/users/tang/data/COIN/* \
+#--num_thread_reader 8 \
+#--cudnn_benchmark 1 \
+#--pin_memory \
+#--checkpoint_dir whl \
+#--batch_size 256 \
+#--batch_size_val 256 \
+#--evaluate \
+#--resume \
+#--horizon 3 \
+#--class_dim 778 \
+#--observation_dim 768 \
+#--dataset coin
 
 
-python3 /scratch/users/tang/Action_diffusion/action_diffusion/action_classifier.py \
---root /scratch/users/tang/data/crosstask_release/* \
---num_thread_reader 8 \
---cudnn_benchmark 1 \
---pin_memory \
---checkpoint_dir whl \
---batch_size 256 \
---batch_size_val 256 \
---evaluate \
---resume \
---horizon 3 \
---class_dim  \
---observation_dim 768 \
---dataset crosstask
+#python3 /scratch/users/tang/Action_diffusion/action_diffusion/action_classifier.py \
+#--root /scratch/users/tang/data/crosstask_release/* \
+#--num_thread_reader 8 \
+#--cudnn_benchmark 1 \
+#--pin_memory \
+#--checkpoint_dir whl \
+#--batch_size 256 \
+#--batch_size_val 256 \
+#--evaluate \
+#--resume \
+#--horizon 3 \
+#--class_dim  \
+#--observation_dim 768 \
+#--dataset crosstask
